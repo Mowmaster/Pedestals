12ac8b6b344227d824a572eae89971c5c6b2cf1c
Mowmaster <mowmaster@outlook.com>
2020-07-09 21:03:21 -0500

    added food filter

---
+++ src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeFilterFood.java
@@ -0,0 +1,53 @@
+package com.mowmaster.pedestals.item.pedestalUpgrades;
+
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static com.mowmaster.pedestals.pedestals.PEDESTALS_TAB;
+import static com.mowmaster.pedestals.references.Reference.MODID;
+
+public class ItemUpgradeFilterFood extends ItemUpgradeBaseFilter
+{
+    public ItemUpgradeFilterFood(Properties builder) {super(builder.group(PEDESTALS_TAB));}
+
+    public void updateAction(int tick, World world, ItemStack itemInPedestal, ItemStack coinInPedestal, BlockPos pedestalPos)
+    {
+
+    }
+
+    @Override
+    public boolean canAcceptItem(World world, BlockPos posPedestal, ItemStack itemStackIn)
+    {
+        boolean returner = false;
+
+        if(itemStackIn.isFood())
+        {
+            returner = true;
+        }
+
+        return returner;
+    }
+
+    public void upgradeAction(World world, BlockPos posOfPedestal, ItemStack coinInPedestal)
+    {
+
+    }
+
+    public static final Item FOOD = new ItemUpgradeFilterFood(new Properties().maxStackSize(64).group(PEDESTALS_TAB)).setRegistryName(new ResourceLocation(MODID, "coin/filterfood"));
+
+    @SubscribeEvent
+    public static void onItemRegistryReady(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(FOOD);
+    }
+
+
+
+}
