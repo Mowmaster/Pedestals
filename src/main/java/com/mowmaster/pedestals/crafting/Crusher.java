f1eab9938671c1258cc4bcf32dfc583fde94bfb4
Mowmaster <mowmaster@outlook.com>
2020-07-19 23:10:47 -0500

    Ready for update hotfix

--- src/main/java/com/mowmaster/pedestals/crafting/Crusher.java
+++ src/main/java/com/mowmaster/pedestals/crafting/Crusher.java
@@ -38,6 +38,44 @@ public class Crusher
         this.addCrusherRecipe(new ItemStack(Items.WHEAT).getItem(),new ItemStack(ItemDust.FLOUR,1));
         this.addCrusherRecipe(new ItemStack(Items.BONE).getItem(),new ItemStack(Items.BONE_MEAL,4));
 
+        //Flowers to Dye
+        //Red Tulip,Poppy,RoseBush(2) = red_dye
+        this.addCrusherRecipe(new ItemStack(Items.RED_TULIP).getItem(),new ItemStack(Items.RED_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.POPPY).getItem(),new ItemStack(Items.RED_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.ROSE_BUSH).getItem(),new ItemStack(Items.RED_DYE,4));
+        //Cactus = green dye
+        this.addCrusherRecipe(new ItemStack(Items.CACTUS).getItem(),new ItemStack(Items.GREEN_DYE,2));
+        //Oxeye, Azure, white tulip = lightgrey
+        this.addCrusherRecipe(new ItemStack(Items.OXEYE_DAISY).getItem(),new ItemStack(Items.LIGHT_GRAY_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.AZURE_BLUET).getItem(),new ItemStack(Items.LIGHT_GRAY_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.WHITE_TULIP).getItem(),new ItemStack(Items.LIGHT_GRAY_DYE,2));
+        //pink tulup, peony(2) = pink dye
+        this.addCrusherRecipe(new ItemStack(Items.PINK_TULIP).getItem(),new ItemStack(Items.PINK_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.PEONY).getItem(),new ItemStack(Items.PINK_DYE,4));
+        //sea pickle = lime dye
+        this.addCrusherRecipe(new ItemStack(Items.SEA_PICKLE).getItem(),new ItemStack(Items.LIME_DYE,2));
+        //dandelion,sunflower(2) = yellow dye
+        this.addCrusherRecipe(new ItemStack(Items.DANDELION).getItem(),new ItemStack(Items.YELLOW_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.SUNFLOWER).getItem(),new ItemStack(Items.YELLOW_DYE,4));
+        //blue orchid = light blue dye
+        this.addCrusherRecipe(new ItemStack(Items.BLUE_ORCHID).getItem(),new ItemStack(Items.LIGHT_BLUE_DYE,2));
+        //lilac(2), allium = magenta dye
+        this.addCrusherRecipe(new ItemStack(Items.LILAC).getItem(),new ItemStack(Items.MAGENTA_DYE,4));
+        this.addCrusherRecipe(new ItemStack(Items.ALLIUM).getItem(),new ItemStack(Items.MAGENTA_DYE,2));
+        //orange tulip = orange dye
+        this.addCrusherRecipe(new ItemStack(Items.ORANGE_TULIP).getItem(),new ItemStack(Items.ORANGE_DYE,2));
+        //corn flower, lapis = blue dye
+        this.addCrusherRecipe(new ItemStack(Items.CORNFLOWER).getItem(),new ItemStack(Items.BLUE_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.LAPIS_LAZULI).getItem(),new ItemStack(Items.BLUE_DYE,2));
+        //cocoa beans = brown dye
+        this.addCrusherRecipe(new ItemStack(Items.COCOA_BEANS).getItem(),new ItemStack(Items.BROWN_DYE,2));
+        //ink sac,wither rose = black dye
+        this.addCrusherRecipe(new ItemStack(Items.INK_SAC).getItem(),new ItemStack(Items.BLACK_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.WITHER_ROSE).getItem(),new ItemStack(Items.BLACK_DYE,2));
+        //lilly of the valey, bonemeal = white dye
+        this.addCrusherRecipe(new ItemStack(Items.LILY_OF_THE_VALLEY).getItem(),new ItemStack(Items.WHITE_DYE,2));
+        this.addCrusherRecipe(new ItemStack(Items.BONE_MEAL).getItem(),new ItemStack(Items.WHITE_DYE,2));
+
     }
 
 
