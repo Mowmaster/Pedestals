27a2a5fb9e944b7a828e387f9d4ed0caa9f6b566
Mowmaster <mowmaster@outlook.com>
2020-08-05 19:13:52 -0500

    the start of some energy machines

---
+++ src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeBaseEnergyMachine.java
@@ -0,0 +1,295 @@
+package com.mowmaster.pedestals.item.pedestalUpgrades;
+
+import com.mowmaster.pedestals.tiles.TilePedestal;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeHooks;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Random;
+
+import static com.mowmaster.pedestals.pedestals.PEDESTALS_TAB;
+import static net.minecraft.state.properties.BlockStateProperties.FACING;
+
+public class ItemUpgradeBaseEnergyMachine extends ItemUpgradeBaseEnergy {
+
+    public final int rfCostPerItemSmelted = 2500;
+
+    public ItemUpgradeBaseEnergyMachine(Properties builder) {super(builder.group(PEDESTALS_TAB));}
+
+    @Override
+    public Boolean canAcceptCapacity() {
+        return true;
+    }
+
+    @Override
+    public int getItemTransferRate(ItemStack stack)
+    {
+        int itemsPerSmelt = 1;
+        switch (getCapacityModifier(stack))
+        {
+            case 0:
+                itemsPerSmelt = 1;
+                break;
+            case 1:
+                itemsPerSmelt=2;
+                break;
+            case 2:
+                itemsPerSmelt = 4;
+                break;
+            case 3:
+                itemsPerSmelt = 8;
+                break;
+            case 4:
+                itemsPerSmelt = 12;
+                break;
+            case 5:
+                itemsPerSmelt=16;
+                break;
+            default: itemsPerSmelt=1;
+        }
+
+        return  itemsPerSmelt;
+    }
+
+    public int getSmeltingSpeed(ItemStack stack)
+    {
+        int smeltingSpeed = 200;
+        switch (intOperationalSpeedModifier(stack))
+        {
+            case 0:
+                smeltingSpeed = 200;//normal speed
+                break;
+            case 1:
+                smeltingSpeed=100;//2x faster
+                break;
+            case 2:
+                smeltingSpeed = 50;//4x faster
+                break;
+            case 3:
+                smeltingSpeed = 33;//6x faster
+                break;
+            case 4:
+                smeltingSpeed = 20;//10x faster
+                break;
+            case 5:
+                smeltingSpeed=10;//20x faster
+                break;
+            default: smeltingSpeed=200;
+        }
+
+        return  smeltingSpeed;
+    }
+
+    public int removeEnergyFuel(TilePedestal pedestal, int amountToRemove, boolean simulate)
+    {
+        int amountToSet = 0;
+        ItemStack coin = pedestal.getCoinOnPedestal();
+        int fuelLeft = getEnergyStored(coin);
+        amountToSet = fuelLeft - amountToRemove;
+        if(amountToRemove >= fuelLeft) amountToSet = -1;
+        if(!simulate)
+        {
+            if(amountToSet == -1) amountToSet = 0;
+            setEnergyStored(coin,amountToSet);
+        }
+
+        return amountToSet;
+    }
+
+    public static int getItemFuelBurnTime(ItemStack fuel)
+    {
+        if (fuel.isEmpty()) return 0;
+        else
+        {
+            int burnTime = ForgeHooks.getBurnTime(fuel);
+            return burnTime;
+        }
+    }
+
+    @Override
+    public void actionOnCollideWithBlock(World world, TilePedestal tilePedestal, BlockPos posPedestal, BlockState state, Entity entityIn)
+    {
+        if(entityIn instanceof ItemEntity)
+        {
+            ItemStack getItemStack = ((ItemEntity) entityIn).getItem();
+            if(getItemFuelBurnTime(getItemStack)>0)
+            {
+                int CurrentBurnTime = tilePedestal.getStoredValueForUpgrades();
+                int getBurnTimeForStack = getItemFuelBurnTime(getItemStack) * getItemStack.getCount();
+                tilePedestal.setStoredValueForUpgrades(CurrentBurnTime + getBurnTimeForStack);
+                if(getItemStack.getItem().equals(Items.LAVA_BUCKET))
+                {
+                    ItemStack getReturned = new ItemStack(Items.BUCKET,getItemStack.getCount());
+                    ItemEntity items1 = new ItemEntity(world, posPedestal.getX() + 0.5, posPedestal.getY() + 1.0, posPedestal.getZ() + 0.5, getReturned);
+                    world.playSound((PlayerEntity) null, posPedestal.getX(), posPedestal.getY(), posPedestal.getZ(), SoundEvents.BLOCK_LAVA_POP, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                    entityIn.remove();
+                    world.addEntity(items1);
+                }
+
+                world.playSound((PlayerEntity) null, posPedestal.getX(), posPedestal.getY(), posPedestal.getZ(), SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                entityIn.remove();
+            }
+        }
+    }
+
+    public String getSmeltingSpeedString(ItemStack stack)
+    {
+        String s3 = "Normal Speed";
+        switch (getSmeltingSpeed(stack))
+        {
+            case 200:
+                s3 = "Normal Speed";
+                break;
+            case 100:
+                s3="2x Faster";
+                break;
+            case 50:
+                s3 = "4x Faster";
+                break;
+            case 33:
+                s3 = "6x Faster";
+                break;
+            case 20:
+                s3 = "10x Faster";
+                break;
+            case 10:
+                s3="20x Faster";
+                break;
+            default: s3= "Normal Speed";
+        }
+        return s3;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void onRandomDisplayTick(TilePedestal pedestal,int tick, BlockState stateIn, World world, BlockPos pos, Random rand)
+    {
+        if(!world.isBlockPowered(pos))
+        {
+            int fuelValue = getEnergyStored(pedestal.getCoinOnPedestal());
+
+            double dx = (double)pos.getX();
+            double dy = (double)pos.getY();
+            double dz = (double)pos.getZ();
+
+            //More than 1 smelt worth
+            if(fuelValue >= rfCostPerItemSmelted)
+            {
+                BlockState state = world.getBlockState(pos);
+                Direction enumfacing = state.get(FACING);
+                BlockPos blockBelow = pos;
+                switch (enumfacing)
+                {
+                    case UP:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.15D, dz+ 0.25D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.15D, dz+ 0.75D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.15D, dz+ 0.25D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.15D, dz+ 0.75D,0, 0, 0);
+                        return;
+                    case DOWN:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+.85D, dz+ 0.25D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+.85D, dz+ 0.75D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+.85D, dz+ 0.25D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+.85D, dz+ 0.75D,0, 0, 0);
+                        return;
+                    case NORTH:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.25D, dz+.85D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.75D, dz+.85D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.25D, dz+.85D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.75D, dz+.85D,0, 0, 0);
+                        return;
+                    case SOUTH:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.25D, dz+0.15D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.75D, dz+0.15D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.25D, dz+0.15D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.75D, dz+0.15D,0, 0, 0);
+                        return;
+                    case EAST:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.15D, dy+ 0.25D, dz+0.25D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.15D, dy+ 0.25D, dz+0.75D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.15D, dy+ 0.75D, dz+0.25D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.15D, dy+ 0.75D, dz+0.75D,0, 0, 0);
+                        return;
+                    case WEST:
+                        if (tick%20 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.85D, dy+0.25D, dz+ 0.25D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.85D, dy+0.25D, dz+ 0.75D,0, 0, 0);
+                        if (tick%15 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.85D, dy+0.75D, dz+ 0.25D,0, 0, 0);
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+0.85D, dy+0.75D, dz+ 0.75D,0, 0, 0);
+                        return;
+                    default:
+                        if (tick%30 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.15D, dz+ 0.25D,0, 0, 0);
+                        if (tick%35 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.25D, dy+0.15D, dz+ 0.75D,0, 0, 0);
+                        if (tick%25 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.15D, dz+ 0.25D,0, 0, 0);
+                        if (tick%40 == 0) world.addParticle(ParticleTypes.FLAME, dx+ 0.75D, dy+0.15D, dz+ 0.75D,0, 0, 0);
+                        return;
+                }
+            }
+        }
+    }
+
+    /*@Override
+    public void chatDetails(PlayerEntity player, TilePedestal pedestal)
+    {
+        ItemStack stack = pedestal.getCoinOnPedestal();
+
+        TranslationTextComponent name = new TranslationTextComponent(getTranslationKey() + ".tooltip_name");
+        name.mergeStyle(TextFormatting.GOLD);
+        player.sendMessage(name,player.getUniqueID());
+
+        TranslationTextComponent rate = new TranslationTextComponent(getTranslationKey() + ".chat_rate");
+        rate.appendString(""+getItemTransferRate(stack)+"");
+        rate.mergeStyle(TextFormatting.GRAY);
+        player.sendMessage(rate,player.getUniqueID());
+
+        //Display Fuel Left
+        int fuelLeft = pedestal.getStoredValueForUpgrades();
+        TranslationTextComponent fuel = new TranslationTextComponent(getTranslationKey() + ".chat_fuel");
+        fuel.appendString("" + fuelLeft/200 + "");
+        fuel.mergeStyle(TextFormatting.GREEN);
+        player.sendMessage(fuel,player.getUniqueID());
+
+        //Display Speed Last Like on Tooltips
+        TranslationTextComponent speed = new TranslationTextComponent(getTranslationKey() + ".chat_speed");
+        speed.appendString(getSmeltingSpeedString(stack));
+        speed.mergeStyle(TextFormatting.RED);
+        player.sendMessage(speed,player.getUniqueID());
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        int s2 = getItemTransferRate(stack);
+        String trr = getSmeltingSpeedString(stack);
+        TranslationTextComponent rate = new TranslationTextComponent(getTranslationKey() + ".tooltip_rate");
+        rate.appendString(""+s2+"");
+        TranslationTextComponent speed = new TranslationTextComponent(getTranslationKey() + ".tooltip_speed");
+        speed.appendString(trr);
+
+        rate.mergeStyle(TextFormatting.GRAY);
+        speed.mergeStyle(TextFormatting.RED);
+
+        tooltip.add(rate);
+        tooltip.add(speed);
+    }*/
+
+}
---
+++ src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeEnergyCrusher.java
@@ -0,0 +1,178 @@
+package com.mowmaster.pedestals.item.pedestalUpgrades;
+
+import com.mowmaster.pedestals.recipes.CrusherRecipe;
+import com.mowmaster.pedestals.tiles.TilePedestal;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.items.IItemHandler;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.mowmaster.pedestals.pedestals.PEDESTALS_TAB;
+import static com.mowmaster.pedestals.references.Reference.MODID;
+
+public class ItemUpgradeEnergyCrusher extends ItemUpgradeBaseEnergyMachine
+{
+    public ItemUpgradeEnergyCrusher(Properties builder) {super(builder.group(PEDESTALS_TAB));}
+
+    @Nullable
+    protected CrusherRecipe getRecipe(World world, ItemStack stackIn) {
+        Inventory inv = new Inventory(stackIn);
+        //System.out.println(world == null ? null : world.getRecipeManager().getRecipe(CrusherRecipe.recipeType, inv, world).orElse(null));
+        return world == null ? null : world.getRecipeManager().getRecipe(CrusherRecipe.recipeType, inv, world).orElse(null);
+    }
+
+    protected Collection<ItemStack> getProcessResults(CrusherRecipe recipe) {
+        //System.out.println((recipe == null)?(Arrays.asList(ItemStack.EMPTY)):(Collections.singleton(recipe.getResult())));
+        return (recipe == null)?(Arrays.asList(ItemStack.EMPTY)):(Collections.singleton(recipe.getResult()));
+    }
+
+
+    public void updateAction(int tick, World world, ItemStack itemInPedestal, ItemStack coinInPedestal, BlockPos pedestalPos)
+    {
+        if(!world.isRemote)
+        {
+            int speed = getSmeltingSpeed(coinInPedestal);
+
+            if(!world.isBlockPowered(pedestalPos))
+            {
+                if (tick%speed == 0) {
+                    upgradeAction(world,pedestalPos,coinInPedestal);
+                }
+            }
+        }
+    }
+
+    public void upgradeAction(World world, BlockPos posOfPedestal, ItemStack coinInPedestal)
+    {
+        BlockPos posInventory = getPosOfBlockBelow(world,posOfPedestal,1);
+        int itemsPerSmelt = getItemTransferRate(coinInPedestal);
+
+        ItemStack itemFromInv = ItemStack.EMPTY;
+        //if(world.getTileEntity(posInventory) !=null)
+        //{
+        LazyOptional<IItemHandler> cap = findItemHandlerAtPos(world,posInventory,getPedestalFacing(world, posOfPedestal),true);
+            if(cap.isPresent())
+            {
+                IItemHandler handler = cap.orElse(null);
+                TileEntity invToPullFrom = world.getTileEntity(posInventory);
+                if(invToPullFrom instanceof TilePedestal) {
+                    itemFromInv = ItemStack.EMPTY;
+
+                }
+                else {
+                    if(handler != null)
+                    {
+                        int i = getNextSlotWithItemsCap(cap,getStackInPedestal(world,posOfPedestal));
+                        if(i>=0)
+                        {
+                            int maxInSlot = handler.getSlotLimit(i);
+                            itemFromInv = handler.getStackInSlot(i);
+                            //Should work without catch since we null check this in our GetNextSlotFunction
+                            Collection<ItemStack> jsonResults = getProcessResults(getRecipe(world,itemFromInv));
+                            //Just check to make sure our recipe output isnt air
+                            ItemStack resultSmelted = (jsonResults.iterator().next().isEmpty())?(ItemStack.EMPTY):(jsonResults.iterator().next());
+                            ItemStack itemFromPedestal = getStackInPedestal(world,posOfPedestal);
+                            if(!resultSmelted.equals(ItemStack.EMPTY))
+                            {
+                                //Null check our slot again, which is probably redundant
+                                if(handler.getStackInSlot(i) != null && !handler.getStackInSlot(i).isEmpty() && handler.getStackInSlot(i).getItem() != Items.AIR)
+                                {
+                                    int roomLeftInPedestal = 64-itemFromPedestal.getCount();
+                                    if(itemFromPedestal.isEmpty() || itemFromPedestal.equals(ItemStack.EMPTY)) roomLeftInPedestal = 64;
+
+                                    //Upgrade Determins ammout of items to smelt, but space count is determined by how much the item smelts into
+                                    int itemInputsPerSmelt = itemsPerSmelt;
+                                    int itemsOutputWhenStackSmelted = (itemInputsPerSmelt*resultSmelted.getCount());
+                                    //Checks to see if pedestal can accept as many items as will be returned on smelt, if not reduce items being smelted
+                                    if(roomLeftInPedestal < itemsOutputWhenStackSmelted)
+                                    {
+                                        itemInputsPerSmelt = Math.floorDiv(roomLeftInPedestal, resultSmelted.getCount());
+                                    }
+                                    //Checks to see how many items are left in the slot IF ITS UNDER the allowedTransferRate then sent the max rate to that.
+                                    if(itemFromInv.getCount() < itemInputsPerSmelt) itemInputsPerSmelt = itemFromInv.getCount();
+
+                                    itemsOutputWhenStackSmelted = (itemInputsPerSmelt*resultSmelted.getCount());
+                                    ItemStack copyIncoming = resultSmelted.copy();
+                                    copyIncoming.setCount(itemsOutputWhenStackSmelted);
+                                    int fuelToConsume = burnTimeCostPerItemSmelted * itemInputsPerSmelt;
+                                    TileEntity pedestalInv = world.getTileEntity(posOfPedestal);
+                                    if(pedestalInv instanceof TilePedestal) {
+                                        TilePedestal ped = ((TilePedestal) pedestalInv);
+                                        //Checks to make sure we have fuel to smelt everything
+                                        if(removeFuel(ped,fuelToConsume,true)>=0)
+                                        {
+                                            handler.extractItem(i,itemInputsPerSmelt ,false );
+                                            removeFuel(ped,fuelToConsume,false);
+                                            world.playSound((PlayerEntity) null, posOfPedestal.getX(), posOfPedestal.getY(), posOfPedestal.getZ(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                                            ped.addItem(copyIncoming);
+                                        }
+                                        //If we done have enough fuel to smelt everything then reduce size of smelt
+                                        else
+                                        {
+                                            //gets fuel left
+                                            int fuelLeft = ped.getStoredValueForUpgrades();
+                                            if(fuelLeft>0)
+                                            {
+                                                //this = a number over 1 unless fuelleft < burnTimeCostPeritemSmelted
+                                                itemInputsPerSmelt = Math.floorDiv(fuelLeft,burnTimeCostPerItemSmelted );
+                                                if(itemInputsPerSmelt >=1)
+                                                {
+                                                    //System.out.println(itemInputsPerSmelt);
+                                                    fuelToConsume = burnTimeCostPerItemSmelted * itemInputsPerSmelt;
+                                                    itemsOutputWhenStackSmelted = (itemInputsPerSmelt*resultSmelted.getCount());
+                                                    copyIncoming.setCount(itemsOutputWhenStackSmelted);
+
+                                                    handler.extractItem(i,itemInputsPerSmelt ,false );
+                                                    removeFuel(ped,fuelToConsume,false);
+                                                    world.playSound((PlayerEntity) null, posOfPedestal.getX(), posOfPedestal.getY(), posOfPedestal.getZ(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                                                    ped.addItem(copyIncoming);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                TileEntity pedestalInv = world.getTileEntity(posOfPedestal);
+                                if(pedestalInv instanceof TilePedestal) {
+                                    TilePedestal ped = ((TilePedestal) pedestalInv);
+                                    if(ped.getItemInPedestal().equals(ItemStack.EMPTY))
+                                    {
+                                        ItemStack copyItemFromInv = itemFromInv.copy();
+                                        handler.extractItem(i,itemFromInv.getCount(),false);
+                                        ped.addItem(copyItemFromInv);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        //}
+    }
+
+    public static final Item CRUSHER = new ItemUpgradeEnergyCrusher(new Properties().maxStackSize(64).group(PEDESTALS_TAB)).setRegistryName(new ResourceLocation(MODID, "coin/crusher"));
+
+    @SubscribeEvent
+    public static void onItemRegistryReady(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(CRUSHER);
+    }
+}
