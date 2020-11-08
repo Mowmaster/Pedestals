e8e8505e7f997abe9e88dd4b09560de136c038d2
Mowmaster <mowmaster@outlook.com>
2020-11-08 02:02:25 -0600

    added a new generator type

---
+++ src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeEnergyGeneratorMob.java
@@ -0,0 +1,383 @@
+package com.mowmaster.pedestals.item.pedestalUpgrades;
+
+import com.mowmaster.pedestals.network.PacketHandler;
+import com.mowmaster.pedestals.network.PacketParticles;
+import com.mowmaster.pedestals.tiles.PedestalTileEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static com.mowmaster.pedestals.pedestals.PEDESTALS_TAB;
+import static com.mowmaster.pedestals.references.Reference.MODID;
+
+public class ItemUpgradeEnergyGeneratorMob extends ItemUpgradeBaseEnergy
+{
+
+    public ItemUpgradeEnergyGeneratorMob(Properties builder) {super(builder.group(PEDESTALS_TAB));}
+
+    @Override
+    public boolean canSendItem(PedestalTileEntity tile)
+    {
+        return tile.getStoredValueForUpgrades()>0;
+    }
+
+    public int getEnergyBuffer(ItemStack stack) {
+        return  40000;
+    }
+
+    public void updateAction(PedestalTileEntity pedestal)
+    {
+        World world = pedestal.getWorld();
+        ItemStack coinInPedestal = pedestal.getCoinOnPedestal();
+        ItemStack itemInPedestal = pedestal.getItemInPedestal();
+        BlockPos pedestalPos = pedestal.getPos();
+
+        if(!world.isRemote)
+        {
+            int getMaxFuelValue = Integer.MAX_VALUE;
+            if(!hasMaxFuelSet(coinInPedestal) || readMaxFuelFromNBT(coinInPedestal) != getMaxFuelValue) {setMaxFuel(coinInPedestal, getMaxFuelValue);}
+
+            if(!world.isBlockPowered(pedestalPos))
+            {
+                //Always send energy, as fast as we can within the Pedestal Energy Network
+                upgradeActionSendEnergy(world,coinInPedestal,pedestalPos);
+                //only run 1 per second
+                    if(getFuelStored(coinInPedestal)>0 && getEnergyStored(coinInPedestal) < getEnergyBuffer(coinInPedestal))
+                    {
+                        if (world.getGameTime()%5 == 0) {
+                            PacketHandler.sendToNearby(world,pedestalPos,new PacketParticles(PacketParticles.EffectType.ANY_COLOR,pedestalPos.getX(),pedestalPos.getY(),pedestalPos.getZ(),145,145,145));
+                        }
+
+                        if (world.getGameTime()%20 == 0) {
+                            upgradeAction(world,pedestalPos,itemInPedestal,coinInPedestal);
+                        }
+                    }
+            }
+        }
+    }
+
+    public void upgradeAction(World world, BlockPos posOfPedestal, ItemStack itemInPedestal, ItemStack coinInPedestal)
+    {
+        /*
+        Okay so basically this generator bases its speed and efficiency off of mobs 'connected' to it
+        It will consume 1 mob drop as fuel as well as damage the corrosponding mob
+        Any mobs can be used, but the more variety and the different types will only improve generation
+
+        speed enchant will make the generator run faster.
+        capacity enchant will modify the base fuel efficiency
+
+        'Connected Mobs' basically means every "cycle" the generator runs, it will check 'below' all its linked pedestals for mob entities,
+        if it finds an entity it will save it to a list, as it builds its list it will also be checking to see if any mobs in the list are similar types,
+        for each different type of mob there will be a modifier that multiplies the base generation. More diverse the mobs, the more energy per tick.
+        {Im thinking 1type: 1.0, 2:2, 3:4, 4:8. 5:16, 6:32. 7:64, 8:128} but i should probably calculate my max vanilla diversity:power combination and set it to a max of 20k/t since thats the max the export can output???
+        ALSO for each mob it will check thats mobs 'difficulty', passive ones will be worth less, while hostile ones will be worth more. this will give a flat additive base power gen.
+        {i might have to make a specific class by class 'base fuel'rate for this. no idea if that could be jsoned, but hopefully???}
+
+        Once the list is built the generator will randomly select a mob from the list, it will get a list of that mobs drops, and then select 1 from the drops(probably the first item in the list).
+        It will then request this item as fuel (Displaying the item above the generator)[maybe also output redstone = to which number in the list the mob is? the list will be in the order of connected pedestals]
+        upon being given an item, it will verify that it was the correct item, if not that item might be voided(maybe??? unless i can dynamically set that in the onCollide)
+        if the correct item was received it will damage the corrosponding entity for 1 heart, and then start to generate power. Once all the 'fuel' has been exhausted it will repeat the list and request process all over again.
+
+        Mobs connected could die, as long as a mob is available before the generator runs its initial check for the cycle, it will be okay.
+         */
+    }
+
+
+
+    public void setFuelStored(ItemStack stack, int fuel)
+    {
+        CompoundNBT compound = new CompoundNBT();
+        if(stack.hasTag())
+        {
+            compound = stack.getTag();
+        }
+
+        compound.putInt("fuel",fuel);
+        stack.setTag(compound);
+    }
+
+    public boolean hasFuel(ItemStack stack)
+    {
+        return getFuelStored(stack)>0;
+    }
+
+    public int getFuelStored(ItemStack stack)
+    {
+        int storedFuel = 0;
+        if(stack.hasTag())
+        {
+            CompoundNBT getCompound = stack.getTag();
+            storedFuel = getCompound.getInt("fuel");
+        }
+        return storedFuel;
+    }
+
+    public void setMaxFuel(ItemStack stack, int amountMax)
+    {
+        writeMaxFuelToNBT(stack,amountMax);
+    }
+
+    public boolean hasMaxFuelSet(ItemStack stack)
+    {
+        boolean returner = false;
+        CompoundNBT compound = new CompoundNBT();
+        if(stack.hasTag())
+        {
+            compound = stack.getTag();
+            if(compound.contains("maxfuel"))
+            {
+                returner = true;
+            }
+        }
+        return returner;
+    }
+
+    public void writeMaxFuelToNBT(ItemStack stack, int value)
+    {
+        CompoundNBT compound = new CompoundNBT();
+        if(stack.hasTag())
+        {
+            compound = stack.getTag();
+        }
+
+        compound.putInt("maxfuel",value);
+        stack.setTag(compound);
+    }
+
+    public int readMaxFuelFromNBT(ItemStack stack)
+    {
+        int maxfuel = 0;
+        if(stack.hasTag())
+        {
+            CompoundNBT getCompound = stack.getTag();
+            maxfuel = getCompound.getInt("maxfuel");
+        }
+        return maxfuel;
+    }
+
+    public boolean addFuel(PedestalTileEntity pedestal, int amountToAdd, boolean simulate)
+    {
+        ItemStack coin = pedestal.getCoinOnPedestal();
+        if(hasMaxFuelSet(coin))
+        {
+            int maxFuel = readMaxFuelFromNBT(coin);
+            int currentFuel = getFuelStored(coin);
+            int addAmount = currentFuel + amountToAdd;
+            if(maxFuel > addAmount)
+            {
+                if(!simulate)
+                {
+                    setFuelStored(coin,addAmount);
+                    pedestal.update();
+                    return true;
+                }
+                //return true if fuel could be added for simulation requests
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean removeFuel(World world, BlockPos posPedestal, int amountToRemove, boolean simulate)
+    {
+        TileEntity entity = world.getTileEntity(posPedestal);
+        if(entity instanceof PedestalTileEntity)
+        {
+            PedestalTileEntity pedestal = (PedestalTileEntity)entity;
+            return removeFuel(pedestal,amountToRemove,simulate);
+        }
+
+        return false;
+    }
+
+    public boolean removeFuel(PedestalTileEntity pedestal, int amountToRemove, boolean simulate)
+    {
+
+        ItemStack coin = pedestal.getCoinOnPedestal();
+        if(hasFuel(coin))
+        {
+            int fuelLeft = getFuelStored(coin);
+            int amountToSet = fuelLeft - amountToRemove;
+            if(fuelLeft >= amountToRemove)
+            {
+                if(!simulate)
+                {
+                    if(amountToSet == -1) amountToSet = 0;
+                    setFuelStored(coin,amountToSet);
+                    pedestal.update();
+                    return true;
+                }
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    //Any item fuel value will be 1600
+    public static int getItemFuelBurnTime(ItemStack fuel)
+    {
+        if (fuel.isEmpty()) return 0;
+        else
+        {
+            return 1600;
+        }
+    }
+
+    @Override
+    public void actionOnCollideWithBlock(World world, PedestalTileEntity tilePedestal, BlockPos posPedestal, BlockState state, Entity entityIn)
+    {
+        if(!world.isRemote)
+        {
+            if(!world.isBlockPowered(posPedestal))
+            {
+                if(entityIn instanceof ItemEntity)
+                {
+                    ItemStack getItemStack = ((ItemEntity) entityIn).getItem();
+                    /*if(getItemFuelBurnTime(getItemStack)>0)
+                    {
+                        int getBurnTimeForStack = getItemFuelBurnTime(getItemStack) * getItemStack.getCount();
+                        if(addFuel(tilePedestal,getBurnTimeForStack,true))
+                        {
+                            addFuel(tilePedestal,getBurnTimeForStack,false);
+                            if(getItemStack.getItem().equals(Items.LAVA_BUCKET))
+                            {
+                                ItemStack getReturned = new ItemStack(Items.BUCKET,getItemStack.getCount());
+                                ItemEntity items1 = new ItemEntity(world, posPedestal.getX() + 0.5, posPedestal.getY() + 1.0, posPedestal.getZ() + 0.5, getReturned);
+                                world.playSound((PlayerEntity) null, posPedestal.getX(), posPedestal.getY(), posPedestal.getZ(), SoundEvents.BLOCK_LAVA_POP, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                                entityIn.remove();
+                                world.addEntity(items1);
+                            }
+
+                            world.playSound((PlayerEntity) null, posPedestal.getX(), posPedestal.getY(), posPedestal.getZ(), SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 0.25F, 1.0F);
+                            entityIn.remove();
+                        }
+                    }*/
+                }
+            }
+        }
+    }
+
+    /*@Override
+    public void chatDetails(PlayerEntity player, PedestalTileEntity pedestal)
+    {
+        ItemStack stack = pedestal.getCoinOnPedestal();
+
+        TranslationTextComponent name = new TranslationTextComponent(getTranslationKey() + ".tooltip_name");
+        name.mergeStyle(TextFormatting.GOLD);
+        player.sendMessage(name, Util.DUMMY_UUID);
+
+        //Display Fuel Left
+        int fuelLeft = getFuelStored(pedestal.getCoinOnPedestal());
+        TranslationTextComponent fuel = new TranslationTextComponent(getTranslationKey() + ".chat_fuel");
+        fuel.appendString("" + fuelLeft + "");
+        fuel.mergeStyle(TextFormatting.DARK_GREEN);
+        player.sendMessage(fuel,Util.DUMMY_UUID);
+
+        TranslationTextComponent xpstored = new TranslationTextComponent(getTranslationKey() + ".chat_rfstored");
+        xpstored.appendString(""+ getEnergyStored(stack) +"");
+        xpstored.mergeStyle(TextFormatting.GREEN);
+        player.sendMessage(xpstored,Util.DUMMY_UUID);
+
+        int opSpeed = getOperationSpeed(stack);
+        double capacityRate = getCapicityModifier(stack);
+        double speedMultiplier = (20/opSpeed);
+        int rfPerTick = (int) (12.5 * speedMultiplier);
+        TranslationTextComponent energyRate = new TranslationTextComponent(getTranslationKey() + ".chat_rfrate");
+        energyRate.appendString(""+ rfPerTick +"");
+        energyRate.mergeStyle(TextFormatting.AQUA);
+        player.sendMessage(energyRate,Util.DUMMY_UUID);
+
+        int capacityRateModified = (int)(Math.round((1.0 - getCapicityModifier(stack))* 100));
+        TranslationTextComponent rate2 = new TranslationTextComponent(getTranslationKey() + ".chat_rfrate2");
+        rate2.appendString("" + capacityRateModified + "%");
+        rate2.mergeStyle(TextFormatting.GRAY);
+        player.sendMessage(rate2,Util.DUMMY_UUID);
+
+        //Display Speed Last Like on Tooltips
+        TranslationTextComponent speed = new TranslationTextComponent(getTranslationKey() + ".chat_speed");
+        speed.appendString(getOperationSpeedString(stack));
+        speed.mergeStyle(TextFormatting.RED);
+        player.sendMessage(speed, Util.DUMMY_UUID);
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        TranslationTextComponent name = new TranslationTextComponent(getTranslationKey() + ".tooltip_name");
+        name.mergeStyle(TextFormatting.GOLD);
+        tooltip.add(name);
+
+        TranslationTextComponent fuelStored = new TranslationTextComponent(getTranslationKey() + ".tooltip_fuelstored");
+        fuelStored.appendString(""+ getFuelStored(stack) +"");
+        fuelStored.mergeStyle(TextFormatting.DARK_GREEN);
+        tooltip.add(fuelStored);
+
+        TranslationTextComponent xpstored = new TranslationTextComponent(getTranslationKey() + ".tooltip_rfstored");
+        //xpstored.appendString()
+        xpstored.appendString(""+ getEnergyStored(stack) +"");
+        //xpstored.mergeStyle(TextFormatting.GREEN)
+        xpstored.mergeStyle(TextFormatting.GREEN);
+        tooltip.add(xpstored);
+
+        TranslationTextComponent xpcapacity = new TranslationTextComponent(getTranslationKey() + ".tooltip_rfcapacity");
+        xpcapacity.appendString(""+ getEnergyBuffer(stack) +"");
+        xpcapacity.mergeStyle(TextFormatting.AQUA);
+        tooltip.add(xpcapacity);
+
+        int opSpeed = getOperationSpeed(stack);
+        double speedMultiplier = (20/opSpeed);
+        int rfPerTick = (int) (12.5 * speedMultiplier);
+        TranslationTextComponent rate = new TranslationTextComponent(getTranslationKey() + ".tooltip_rate");
+        rate.appendString("" + rfPerTick + "");
+        rate.mergeStyle(TextFormatting.GRAY);
+        tooltip.add(rate);
+
+        int capacityRate = (int)(Math.round((1.0 - getCapicityModifier(stack))* 100));
+        TranslationTextComponent rate2 = new TranslationTextComponent(getTranslationKey() + ".tooltip_rate2");
+        rate2.appendString("" + capacityRate + "%");
+        rate2.mergeStyle(TextFormatting.DARK_GRAY);
+        tooltip.add(rate2);
+
+        TranslationTextComponent speed = new TranslationTextComponent(getTranslationKey() + ".tooltip_speed");
+        speed.appendString(getOperationSpeedString(stack));
+        speed.mergeStyle(TextFormatting.RED);
+        tooltip.add(speed);
+    }*/
+
+    public static final Item RFMOBGEN = new ItemUpgradeEnergyGeneratorMob(new Properties().maxStackSize(64).group(PEDESTALS_TAB)).setRegistryName(new ResourceLocation(MODID, "coin/rfmobgen"));
+
+    @SubscribeEvent
+    public static void onItemRegistryReady(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(RFMOBGEN);
+    }
+
+
+}
