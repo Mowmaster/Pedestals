9bc3cc3a5d4d3691a1d0ccab131f56bfd0dd0c6e
Mowmaster <mowmaster@outlook.com>
2020-11-09 00:45:27 -0600

    updated the magnet to also have a range value seperate from the height

--- src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeEffectMagnet.java
+++ src/main/java/com/mowmaster/pedestals/item/pedestalUpgrades/ItemUpgradeEffectMagnet.java
@@ -39,12 +39,49 @@ public class ItemUpgradeEffectMagnet extends ItemUpgradeBase
         return true;
     }
 
+    @Override
+    public Boolean canAcceptRange() {return true;}
+
     public int getAreaWidth(ItemStack stack)
     {
         int areaWidth = 0;
         int aW = getAreaModifier(stack);
         areaWidth = ((aW)+1);
         return  areaWidth;
+    }
+
+    public int getRangeHeight(ItemStack stack)
+    {
+        return getHeight(stack);
+    }
+
+    public int getHeight(ItemStack stack)
+    {
+        int height = 3;
+        switch (getRangeModifier(stack))
+        {
+            case 0:
+                height = 3;
+                break;
+            case 1:
+                height=5;
+                break;
+            case 2:
+                height = 7;
+                break;
+            case 3:
+                height = 9;
+                break;
+            case 4:
+                height = 11;
+                break;
+            case 5:
+                height=13;
+                break;
+            default: height=3;
+        }
+
+        return  height;
     }
 
     @Override
@@ -56,7 +93,7 @@ public class ItemUpgradeEffectMagnet extends ItemUpgradeBase
     @Override
     public int[] getWorkAreaY(World world, BlockPos pos, ItemStack coin)
     {
-        return new int[]{((2*getAreaWidth(coin))+1),0};
+        return new int[]{getRangeHeight(coin),0};
     }
 
     @Override
@@ -87,7 +124,8 @@ public class ItemUpgradeEffectMagnet extends ItemUpgradeBase
     public void upgradeAction(World world, ItemStack itemInPedestal, ItemStack coinInPedestal, BlockPos posOfPedestal)
     {
         int width = getAreaWidth(coinInPedestal);
-        int height = (2*width)+1;
+        int height = getRangeHeight(coinInPedestal);
+
         BlockPos negBlockPos = getNegRangePosEntity(world,posOfPedestal,width,height);
         BlockPos posBlockPos = getPosRangePosEntity(world,posOfPedestal,width,height);
 
@@ -151,11 +189,12 @@ public class ItemUpgradeEffectMagnet extends ItemUpgradeBase
 
         int s3 = getAreaWidth(stack);
         String tr = "" + (s3+s3+1) + "";
+        String trr = "" + getRangeHeight(stack) + "";
         TranslationTextComponent area = new TranslationTextComponent(getTranslationKey() + ".chat_area");
         TranslationTextComponent areax = new TranslationTextComponent(getTranslationKey() + ".chat_areax");
         area.appendString(tr);
         area.appendString(areax.getString());
-        area.appendString(tr);
+        area.appendString(trr);
         area.appendString(areax.getString());
         area.appendString(tr);
         area.mergeStyle(TextFormatting.WHITE);
@@ -175,11 +214,12 @@ public class ItemUpgradeEffectMagnet extends ItemUpgradeBase
 
         int s3 = getAreaWidth(stack);
         String tr = "" + (s3+s3+1) + "";
+        String trr = "" + getRangeHeight(stack) + "";
         TranslationTextComponent area = new TranslationTextComponent(getTranslationKey() + ".tooltip_area");
         TranslationTextComponent areax = new TranslationTextComponent(getTranslationKey() + ".tooltip_areax");
         area.appendString(tr);
         area.appendString(areax.getString());
-        area.appendString(tr);
+        area.appendString(trr);
         area.appendString(areax.getString());
         area.appendString(tr);
         TranslationTextComponent speed = new TranslationTextComponent(getTranslationKey() + ".tooltip_speed");
