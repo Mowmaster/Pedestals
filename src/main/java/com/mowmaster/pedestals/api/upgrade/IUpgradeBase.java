6e23e539b89d0a6ad3148947569c3810c0d69908
Mowmaster <mowmaster@outlook.com>
2021-07-19 23:18:50 -0500

    start on a pedestals api

---
+++ src/main/java/com/mowmaster/pedestals/api/upgrade/IUpgradeBase.java
@@ -0,0 +1,12 @@
+package com.mowmaster.pedestals.api.upgrade;
+
+//Chat with InfinityRader about API's
+//https://github.com/AgriCraft/AgriCraft/blob/master/src/main/java/com/infinityraider/agricraft/api/v1/crop/IAgriCrop.java
+//https://discord.com/channels/230701400080777227/230709955324280832/866772319312543764
+//https://discord.com/channels/230701400080777227/230709955324280832/866776324420272158
+
+//Chat with Loth about API's
+//https://discord.com/channels/749302798797242449/749302799644229715/866807219550814238
+
+public interface IUpgradeBase {
+}
