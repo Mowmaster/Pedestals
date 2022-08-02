9cfad5b96b9b44204c7fd7e4eccc20efeeb1422c
Mowmaster <mowmaster@outlook.com>
2022-08-02 00:27:05 -0500

    made sure the filter inventory stopped showing 2 empty slots when no items are in the filter

--- src/main/java/com/mowmaster/pedestals/client/ClientItemTooltipComponent.java
+++ src/main/java/com/mowmaster/pedestals/client/ClientItemTooltipComponent.java
@@ -29,11 +29,11 @@ public class ClientItemTooltipComponent  implements ClientTooltipComponent
     }
 
     public int getHeight() {
-        return this.gridSizeY() * 20 + 2 + 4;
+        return (items.size()>0)?(this.gridSizeY() * 20 + 2 + 4):(0);
     }
 
     public int getWidth(Font p_169901_) {
-        return this.gridSizeX() * 18 + 2;
+        return (items.size()>0)?(this.gridSizeX() * 18 + 2):(0);
     }
 
     public void renderImage(Font p_194042_, int p_194043_, int p_194044_, PoseStack p_194045_, ItemRenderer p_194046_, int p_194047_) {
@@ -54,50 +54,59 @@ public class ClientItemTooltipComponent  implements ClientTooltipComponent
     }
 
     private void renderSlot(int p_194027_, int p_194028_, int p_194029_, boolean p_194030_, Font p_194031_, PoseStack p_194032_, ItemRenderer p_194033_, int p_194034_) {
-        if (p_194029_ >= this.items.size()) {
-            this.blit(p_194032_, p_194027_, p_194028_, p_194034_, p_194030_ ? Texture.BLOCKED_SLOT : Texture.SLOT);
-        } else {
-            ItemStack itemstack = this.items.get(p_194029_);
-            this.blit(p_194032_, p_194027_, p_194028_, p_194034_, Texture.SLOT);
-            p_194033_.renderAndDecorateItem(itemstack, p_194027_ + 1, p_194028_ + 1, p_194029_);
-            p_194033_.renderGuiItemDecorations(p_194031_, itemstack, p_194027_ + 1, p_194028_ + 1);
-            if (p_194029_ == 0) {
-                AbstractContainerScreen.renderSlotHighlight(p_194032_, p_194027_ + 1, p_194028_ + 1, p_194034_);
-            }
+        if(items.size()>0)
+        {
+            if (p_194029_ >= this.items.size()) {
+                this.blit(p_194032_, p_194027_, p_194028_, p_194034_, p_194030_ ? Texture.BLOCKED_SLOT : Texture.SLOT);
+            } else {
+                ItemStack itemstack = this.items.get(p_194029_);
+                this.blit(p_194032_, p_194027_, p_194028_, p_194034_, Texture.SLOT);
+                p_194033_.renderAndDecorateItem(itemstack, p_194027_ + 1, p_194028_ + 1, p_194029_);
+                p_194033_.renderGuiItemDecorations(p_194031_, itemstack, p_194027_ + 1, p_194028_ + 1);
+                if (p_194029_ == 0) {
+                    AbstractContainerScreen.renderSlotHighlight(p_194032_, p_194027_ + 1, p_194028_ + 1, p_194034_);
+                }
 
+            }
         }
     }
 
     private void drawBorder(int p_194020_, int p_194021_, int p_194022_, int p_194023_, PoseStack p_194024_, int p_194025_) {
-        this.blit(p_194024_, p_194020_, p_194021_, p_194025_, Texture.BORDER_CORNER_TOP);
-        this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_, p_194025_, Texture.BORDER_CORNER_TOP);
+        if(items.size()>0)
+        {
+            this.blit(p_194024_, p_194020_, p_194021_, p_194025_, Texture.BORDER_CORNER_TOP);
+            this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_, p_194025_, Texture.BORDER_CORNER_TOP);
 
-        for(int i = 0; i < p_194022_; ++i) {
-            this.blit(p_194024_, p_194020_ + 1 + i * 18, p_194021_, p_194025_, Texture.BORDER_HORIZONTAL_TOP);
-            this.blit(p_194024_, p_194020_ + 1 + i * 18, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_HORIZONTAL_BOTTOM);
+            for(int i = 0; i < p_194022_; ++i) {
+                this.blit(p_194024_, p_194020_ + 1 + i * 18, p_194021_, p_194025_, Texture.BORDER_HORIZONTAL_TOP);
+                this.blit(p_194024_, p_194020_ + 1 + i * 18, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_HORIZONTAL_BOTTOM);
+            }
+
+            for(int j = 0; j < p_194023_; ++j) {
+                this.blit(p_194024_, p_194020_, p_194021_ + j * 20 + 1, p_194025_, Texture.BORDER_VERTICAL);
+                this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_ + j * 20 + 1, p_194025_, Texture.BORDER_VERTICAL);
+            }
+
+            this.blit(p_194024_, p_194020_, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_CORNER_BOTTOM);
+            this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_CORNER_BOTTOM);
         }
-
-        for(int j = 0; j < p_194023_; ++j) {
-            this.blit(p_194024_, p_194020_, p_194021_ + j * 20 + 1, p_194025_, Texture.BORDER_VERTICAL);
-            this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_ + j * 20 + 1, p_194025_, Texture.BORDER_VERTICAL);
-        }
-
-        this.blit(p_194024_, p_194020_, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_CORNER_BOTTOM);
-        this.blit(p_194024_, p_194020_ + p_194022_ * 18 + 1, p_194021_ + p_194023_ * 20, p_194025_, Texture.BORDER_CORNER_BOTTOM);
     }
 
     private void blit(PoseStack p_194036_, int p_194037_, int p_194038_, int p_194039_, Texture p_194040_) {
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
-        GuiComponent.blit(p_194036_, p_194037_, p_194038_, p_194039_, (float)p_194040_.x, (float)p_194040_.y, p_194040_.w, p_194040_.h, 128, 128);
+        if(items.size()>0)
+        {
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
+            GuiComponent.blit(p_194036_, p_194037_, p_194038_, p_194039_, (float)p_194040_.x, (float)p_194040_.y, p_194040_.w, p_194040_.h, 128, 128);
+        }
     }
 
     private int gridSizeX() {
-        return Math.max(2, (int)Math.ceil(Math.sqrt((double)this.items.size() + 1.0D)));
+        return (items.size()>0)?(Math.max(2, (int)Math.ceil(Math.sqrt((double)this.items.size() + 1.0D)))):(0);
     }
 
     private int gridSizeY() {
-        return (int)Math.ceil(((double)this.items.size() + 1.0D) / (double)this.gridSizeX());
+        return (items.size()>0)?((int)Math.ceil(((double)this.items.size() + 1.0D) / (double)this.gridSizeX())):(0);
     }
 
     @OnlyIn(Dist.CLIENT)
